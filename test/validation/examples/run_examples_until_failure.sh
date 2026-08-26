dc822a14656a91fd9b7f69c5fb8d2cd28147d20c
tlplayer <timothy.player@comcast.net>
2026-08-25 22:01:13 -0400

    fix test script

--- test/validation/examples/run_examples_until_failure.sh
+++ test/validation/examples/run_examples_until_failure.sh
@@ -1,4 +1,10 @@
-find docs/examples -type f -name '*.sev' -print0 | sort -z | while IFS= read -r -d '' f; do
-    echo "=== $f ==="
-    sev test "$f" || break
+ROOT="docs/examples/"
+
+find "$ROOT" -type f -name '*.sev' -printf '%h\0' | sort -zu | while IFS= read -r -d '' dir; do
+    echo "=== $dir ==="
+    (
+        cd "$dir" || exit 1
+        sev test
+        sev test --mutate
+    )
 done
